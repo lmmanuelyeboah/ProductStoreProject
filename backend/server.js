4e3ab71cab5d0a7fb6c896ba253ebecbe84ab268
lmmanuelyeboah <lmmanuelyeboah98@gmail.com>
2025-09-23 00:45:49 +0000

    about to connet to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+app.get("/", (req, res) => {});
+console.log(process.env.MONGO_URI);
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  console.log("server started at http://localhost:3000");
 });
